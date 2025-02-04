da7df3908f356c3ce16b0f0fbca44dc0da30fc30
Evandro Pomatti <evandro.pomatti@gmail.com>
2025-02-04 19:07:42 -0300

    feat: enterprise policy

--- modules/enterprise-policy/main.tf
+++ modules/enterprise-policy/main.tf
@@ -12,29 +12,28 @@ resource "azapi_resource" "network_injection_enterprise_policy" {
   location  = var.powerapps_location
   parent_id = var.resource_group_id
 
-  # https://github.com/Azure/bicep-types-az/issues/2251
-  # schema_validation_enabled = false
+  # This seems to be a bug for now: https://github.com/Azure/bicep-types-az/issues/2251
+  schema_validation_enabled = false
 
-  body = jsonencode({
-    "properties" : {
-      "networkInjection" : {
-        "virtualNetworks" : [
+  body = {
+    properties = {
+      networkInjection = {
+        virtualNetworks = [
           {
-            "id" : "${var.primary_vnet_id}",
-            "subnet" : {
-              "name" : "${var.primary_subnet_name}"
+            id = "${var.primary_vnet_id}"
+            subnet = {
+              name = "${var.primary_subnet_name}"
             }
           },
           {
-            "id" : "${var.secondary_vnet_id}",
-            "subnet" : {
-              "name" : "${var.secondary_subnet_name}"
+            id = "${var.secondary_vnet_id}",
+            subnet = {
+              name = "${var.secondary_subnet_name}"
             }
           }
         ]
       }
-    },
-    "kind" : "NetworkInjection"
-  })
-
+    }
+    kind = "NetworkInjection"
+  }
 }
