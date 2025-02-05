28bd7edd511e57fd233af606f6f6f0ea0fddf48f
Evandro Pomatti <evandro.pomatti@gmail.com>
2025-02-04 22:02:35 -0300

    feat: outputs

---
+++ modules/powerapps/enterprise-policy/output.tf
@@ -0,0 +1,3 @@
+output "policy_resource_id" {
+  value = azapi_resource.network_injection_enterprise_policy.id
+}
--- output.tf
+++ output.tf
@@ -1,14 +1,18 @@
-# output "mssql_fully_qualified_domain_name" {
-#   value = module.mssql.server_fqdn
-# }
+output "mssql_fully_qualified_domain_name" {
+  value = module.mssql.server_fqdn
+}
 
-# output "mssql_database_name" {
-#   value = module.mssql.database_name
-# }
+output "mssql_database_name" {
+  value = module.mssql.database_name
+}
 
-# output "powerapps_sqlservice_service_principal_display_name" {
-#   value = module.entra.powerapps_sqlservice_service_principal_display_name
-# }
+output "powerapps_sqlservice_service_principal_display_name" {
+  value = module.entra.powerapps_sqlservice_service_principal_display_name
+}
+
+output "powerapps_enterprise_policy_resource_id" {
+  value = module.enterprise_policy.policy_resource_id
+}
 
 # output "gateway_public_ip_address" {
 #   value = var.create_gateway == true ? module.gateway.public_ip_address : null
